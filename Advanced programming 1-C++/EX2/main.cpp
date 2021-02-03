756c2398e35cb09e440bdbc87f9485f3fb06e576
liz311 <lizasis20@gmail.com>
2021-02-03 18:42:07 +0200

    Create main.cpp

---
+++ Advanced programming 1-C++/EX2/main.cpp
@@ -0,0 +1,17 @@
+/* Created By Lizzy Asis */
+
+#include "Matrix.h"
+
+void main()
+{
+	Matrix m(2, 2);
+	m[0][0] = 2;
+	m[1][1] = 2;
+	cout << m << endl;
+	m = m;
+	const Matrix s = -m;
+	cout << m << endl << s << endl;
+	m = s + 2 * -m * m * 2 - s;
+	cout << m << endl << s << endl;
+	cout << s[1][1] << endl;
+}
